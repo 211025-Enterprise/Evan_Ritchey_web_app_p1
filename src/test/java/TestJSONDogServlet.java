c25958ddd9ecd059d4d39d979492e36ea8c93a85
eritcheygmu <55561283+eritcheygmu@users.noreply.github.com>
2021-11-24 00:28:44 -0500

    added a tester for DogServlet

---
+++ src/test/java/TestJSONDogServlet.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class TestJSONDogServlet {
+
+    @Test
+    public void testCreate(){
+
+    }
+
+    @Test
+    public void testGet(){
+
+    }
+
+    @Test
+    public void testUpdate(){
+
+    }
+
+    @Test
+    public void testDelete(){
+
+    }
+}
